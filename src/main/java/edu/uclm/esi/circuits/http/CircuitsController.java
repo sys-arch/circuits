b5cd830c32be6dc7722fd7d208301e7d12181b3c
‎WM <91469450+sys-arch@users.noreply.github.com>
2025-02-18 08:39:45 +0100

    Cambios 17/02/25

--- src/main/java/edu/uclm/esi/circuits/http/CircuitsController.java
+++ src/main/java/edu/uclm/esi/circuits/http/CircuitsController.java
@@ -3,12 +3,10 @@ package edu.uclm.esi.circuits.http;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import edu.uclm.esi.circuits.services.CircuitService;
 
@@ -19,12 +17,12 @@ public class CircuitsController {
     @Autowired
     private CircuitService service;
 
-    @GetMapping("/createCircuit")
-    public Map<String,Object> postCircuit(@RequestParam int qubits) {
-        if (qubits < 1) {
+    @PostMapping("/createCircuit")
+    public String postCircuit(@RequestBody Map<String, Object> body) {
+        /*if (qubits < 1) {
             throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, "The number of qubits must be greater than 0");
-        }
+        }*/
         
-        return this.service.createCircuit(qubits);
+        return this.service.createCircuit(body);
     }
 }
--- src/main/java/edu/uclm/esi/circuits/services/CircuitService.java
+++ src/main/java/edu/uclm/esi/circuits/services/CircuitService.java
@@ -3,11 +3,14 @@ package edu.uclm.esi.circuits.services;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CircuitService {
 
-    public Map<String, Object> createCircuit(int qubits) {
+    public String createCircuit(Map<String, Object> body) {
         Map<String, Object> result = new HashMap<>();
-        result.put("mensaje", "Creating a circuit with " + qubits + " qubits");
-        return result;
+        //result.put("mensaje", "Creating a circuit with " + qubits + " qubits");
+        return "Hola";
     }
 }
